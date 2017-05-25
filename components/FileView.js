737a3e3db4fd450c481878d090f5fe82946ef30c
Jordan Sleeper <ezirens@gmail.com>
2017-05-25 16:36:48 +0000

    Done.

--- components/FileView.js
+++ components/FileView.js
@@ -5,8 +5,19 @@ import Toolbar from './Toolbar';
 
 export default class FileView extends React.Component {
   render() {
+    const { file, onChange, onAdd, onRemove } = this.props;
     return (
-      <div>
+      <div className='file-view'>
+        <Toolbar
+          onAdd={onAdd}
+          onRemove={onRemove}
+        />
+        <textarea
+          className='file-view__text'
+          placeholder='type some text here...'
+          value={file}
+          onChange={onChange}
+        />
       </div>
     );
   }
