b5b8ad4ae89400f9fa810540cb11f17859ab5877
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-31 03:37:50 +0000

    Done.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,13 +4,33 @@ import React from 'react';
 import SidebarItem from './SidebarItem';
 
 export default class Sidebar extends React.Component {
-  handleClick(index, ev) {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(index, ev) {
+    ev.preventDefault();
+    this.props.onSelect(index)
+  }
+
   render() {
-    const { files, selectedFileIndex, onAdd } = this.props;
+    const { files, selectedFileIndex } = this.props;
 
     return (
       <ul className="sidebar">
+        {
+          files.map((item, i) => (
+            <SidebarItem
+              isSelected={selectedFileIndex === i}
+              key={i}
+              file={item}
+              onClick={this.handleClick.bind(null, i)}
+            />
+          ))
+        }
+
       </ul>
     );
   }
